5ee4771f4782049eba5fb4625b23964c80fce89f
김호연 <sgm00006@gmail.com>
2025-04-15 16:09:31 +0900

    initial commit
    초기 커밋

---
+++ coupon-system-tutorial/src/main/java/com/example/coupon_system_tutorial/CouponSystemTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.coupon_system_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponSystemTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponSystemTutorialApplication.class, args);
+	}
+
+}
---
+++ coupon-system-tutorial/src/test/java/com/example/coupon_system_tutorial/CouponSystemTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coupon_system_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponSystemTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
